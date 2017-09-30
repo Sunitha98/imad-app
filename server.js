a242fd16976c3631ab8620f765dab7f61fb521d9
Sunitha98 <32041972+Sunitha98@users.noreply.github.com>
2017-09-30 15:44:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 app.get('/article-one',function(req,res)
 {
-    res.send("article one has opened and served here");
+      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res)
